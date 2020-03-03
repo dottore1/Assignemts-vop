0427d572e89c3f88861d7f885e7c1bc452263dc4
Nichlas Boraso <nichlasboraso@gmail.com>
2020-03-03 22:03:38 +0100

    upload

---
+++ Lesson03PreparationExercises/Lesson03PreparationExercises/src/main/java/comparable_exercise/Person.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package comparable_exercise;
+
+import java.util.*;
+
+/**
+ * @author erso
+ */
+public class Person implements Comparable<Person> {
+    private String fName;
+    private String lName;
+    private GregorianCalendar birthDay;
+    private double heigth;
+
+    public Person(String fName, String lName, int bYear, int bMonth, int bDate, double heigth) {
+        this.fName = fName;
+        this.lName = lName;
+        this.birthDay = new GregorianCalendar(bYear, bMonth, bDate);
+        this.heigth = heigth;
+    }
+
+    public static void main(String[] args) {
+        List<Person> list = new ArrayList();
+        list.add(new Person("A", "BB", 1980, 3, 17, 1.87));
+        list.add(new Person("B", "BB", 1980, 3, 8, 1.86));
+        list.add(new Person("A", "AA", 1980, 3, 9, 1.67));
+        list.add(new Person("A", "BB", 1980, 3, 10, 1.67));
+        list.add(new Person("A", "BB", 1980, 3, 1, 1.66));
+        list.add(new Person("A", "CC", 1980, 3, 1, 1.65));
+
+        System.out.println(list);
+
+        Collections.sort(list);
+        System.out.println("\nsorted:\n" + list);
+
+        Comparator<Person> comp = new Comparator<Person>() {
+            // Opgave 1B:
+            // Comparatoren skal sorterer på heigth
+            @Override
+            public int compare(Person o1, Person o2) {
+                if (o1.heigth < o2.heigth) {
+                    return -1;
+                } else if (o1.heigth == o2.heigth) {
+                    return 0;
+                } else {
+                    return 1;
+                }
+
+            }
+
+
+        };
+
+        // Fjern udkommenteringen, når Comparatoren er programmeret:
+
+        Collections.sort(list, comp);
+        System.out.println("\nsorted:\n" + list);
+
+
+    }
+
+    @Override
+    public String toString() {
+        return "fName=" + fName + ", lName=" + lName + ", birthDay=" + birthDay.getTime() + ", height_" + heigth + '\n';
+    }
+
+    //Opgave 1A:
+    // Der skal sorteres på efternavn. Hvis ens, skal der sorteres på fornavn.
+    // Hvis det stadig er ens sorteres på fødselsdag.
+    @Override
+    public int compareTo(Person o) {
+        if (this.lName != o.lName) {
+            return this.lName.compareTo(o.lName);
+        } else {
+            if (this.fName != o.fName) {
+                return this.fName.compareTo(o.fName);
+            } else {
+                return this.birthDay.compareTo(o.birthDay);
+            }
+        }
+
+    }
+
+
+}
